209060eda1dc0fddca9ea810b6c267b32e02a475
ABRJrocks <40339959+ABRJrocks@users.noreply.github.com>
2022-07-14 02:59:33 +0500

    database updated

--- database/rhaerp.sql
+++ database/rhaerp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2022 at 12:14 AM
+-- Generation Time: Jul 13, 2022 at 11:59 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -68,8 +68,27 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`c_id`, `c_name`, `crh`, `semester`) VALUES
+(0, 'None', 0, 0),
 (1, 'OOP', 3, 3),
-(2, 'Software Engineering', 3, 4);
+(2, 'Software Engineering', 3, 4),
+(3, 'DBS', 4, 4),
+(5, 'TBW', 3, 4),
+(6, 'IBQ', 2, 3),
+(7, 'IICT', 4, 1),
+(8, 'PF', 4, 1),
+(9, 'Discrete Structures', 3, 1),
+(10, 'Calculus', 3, 1),
+(11, 'MVC', 3, 2),
+(12, 'Prob & Stats', 3, 2),
+(13, 'DSA', 4, 3),
+(14, 'COAL', 4, 4),
+(15, 'Web Programming', 3, 4),
+(16, 'COA', 3, 3),
+(17, 'Pak Std.', 3, 2),
+(18, 'IST', 3, 1),
+(19, 'English Composition', 3, 1),
+(20, 'DLD', 3, 2),
+(21, 'IHS', 3, 4);
 
 -- --------------------------------------------------------
 
@@ -112,7 +131,39 @@ CREATE TABLE `std` (
 --
 
 INSERT INTO `std` (`fname`, `lname`, `sap_id`, `semester`, `email`) VALUES
-('Abdul', 'Rafay', 25601, '1', '');
+('Abdul', 'Rafay', 25602, '5', 'abdulrafeh380@gmail.com'),
+('Ahmad', 'Raza', 25603, '3', 'ahmadraza123@gmail.com'),
+('Husnain', 'Sajid', 25604, '3', 'hassi@gmail.com'),
+('Fahad', 'Khan', 25606, '5', 'fahadkhan@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `std_enroll_course`
+--
+
+CREATE TABLE `std_enroll_course` (
+  `sap_id` int(11) NOT NULL,
+  `std_name` varchar(100) NOT NULL,
+  `crs1` int(11) NOT NULL,
+  `crs2` int(11) NOT NULL,
+  `crs3` int(11) NOT NULL,
+  `crs4` int(11) NOT NULL,
+  `crs5` int(11) NOT NULL,
+  `crs6` int(11) NOT NULL,
+  `sr` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `std_enroll_course`
+--
+
+INSERT INTO `std_enroll_course` (`sap_id`, `std_name`, `crs1`, `crs2`, `crs3`, `crs4`, `crs5`, `crs6`, `sr`) VALUES
+(25604, 'Husnain Sajid', 2, 1, 3, 3, 2, 1, 1),
+(25602, 'Abdul Rafay', 3, 2, 1, 3, 3, 2, 2),
+(25603, 'Ahmad Raza', 1, 3, 2, 1, 2, 3, 3),
+(25603, 'Ahmad Raza', 0, 0, 0, 0, 0, 0, 4),
+(25606, 'Fahad Khan', 2, 1, 3, 0, 0, 0, 5);
 
 --
 -- Indexes for dumped tables
@@ -137,6 +188,12 @@ ALTER TABLE `std`
   ADD PRIMARY KEY (`sap_id`);
 
 --
+-- Indexes for table `std_enroll_course`
+--
+ALTER TABLE `std_enroll_course`
+  ADD PRIMARY KEY (`sr`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -150,7 +207,19 @@ ALTER TABLE `assign_course`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `c_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `c_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `std`
+--
+ALTER TABLE `std`
+  MODIFY `sap_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25607;
+
+--
+-- AUTO_INCREMENT for table `std_enroll_course`
+--
+ALTER TABLE `std_enroll_course`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
