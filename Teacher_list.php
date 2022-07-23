26984c928a3685ab1b454aed96bbf934226d469f
ABRJrocks <40339959+ABRJrocks@users.noreply.github.com>
2022-07-24 00:17:00 +0500

    Time table creation

--- Teacher_list.php
+++ Teacher_list.php
@@ -1,4 +1,8 @@
-<?php include 'connection.php' ?>
+<?php
+include 'connection.php';
+session_start();
+if($_SESSION["fname"]) { 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -94,6 +98,12 @@
     </footer>
     </div>
     </div>
+    <?php 
+}
+else {
+  include ('login.php');
+}
+ ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="js/scripts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
