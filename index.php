1163aa65db98209493099a029f1061ef72d8d88f
Hussy24 <hasajid882@gmail.com>
2022-07-27 00:55:56 +0500

    dd

--- index.php
+++ index.php
@@ -26,12 +26,11 @@ if($_SESSION["fname"]) {
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Dashboard</h1>
+                    <h1 class="mt-4" style="color:#34495e">Dashboard</h1>
                     <div class="row">
                         <div class="col col-md-1"></div>
                         <div class="col col-md-2">
                             <div class="card" style="width: 18rem; ">
-                                <img src="assets/1st.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title">Student List</h5>
                                     <p class="card-text">Click on the button below to see list of the students</p>
@@ -42,7 +41,7 @@ if($_SESSION["fname"]) {
                         <div class="col col-dash col-md-1"></div>
                         <div class="col col-md-2">
                             <div class="card" style="width: 18rem;">
-                                <img src="assets/2nd.jpg" style="height: 193px;" class="card-img-top" alt="...">
+                               
                                 <div class="card-body">
                                     <h5 class="card-title">Teachers List</h5>
                                     <p class="card-text">Click on the button below to show list of All Teachers.
@@ -54,11 +53,43 @@ if($_SESSION["fname"]) {
                         <div class="col col-dash col-md-1"></div>
                         <div class="col col-md-2">
                             <div class="card" style="width: 18rem;">
-                                <img src="assets/3rd.jpg" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title">Enrolled Student Courses</h5>
                                     <p class="card-text">Click on the button below to display enrolled courses of Students</p>
                                     <a href="enrolled_courses.php" class="btn btn-primary" style="background-color:#34495e">Continue</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                        <div class="col col-md-1"></div>
+                        <div class="col col-md-2">
+                            <div class="card" style="width: 18rem; ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Student List</h5>
+                                    <p class="card-text">Click on the button below to see list of the students</p>
+                                    <a href="student_list.php" class="btn btn-primary" style="background-color:#34495e">Continue</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-dash col-md-1"></div>
+                        <div class="col col-md-2">
+                            <div class="card" style="width: 18rem;">
+                               
+                                <div class="card-body">
+                                    <h5 class="card-title">Teachers List</h5>
+                                    <p class="card-text">Click on the button below to show list of All Teachers.
+                                    </p>
+                                    <a href="teacher_list.php" class="btn btn-primary" style="background-color:#34495e">continue</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-dash col-md-1"></div>
+                        <div class="col col-md-2">
+                            <div class="card" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Show Teacher Information</h5>
+                                    <p class="card-text">Click on the button below to see Teachers Information</p>
+                                    <a href="all_teach_list.php" class="btn btn-primary" style="background-color:#34495e">Continue</a>
                                 </div>
                             </div>
                         </div>
